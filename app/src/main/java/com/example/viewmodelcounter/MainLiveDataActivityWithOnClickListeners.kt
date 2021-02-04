6f253f86ba8676f225006077ac37e281d2eaf323
petrivesikivi <petri.vesikivi@gmail.com>
2021-02-04 11:11:53 +0200

    4th version of Activity added: MainLiveDataActivityWithOnClickListeners.kt

--- app/src/main/java/com/example/viewmodelcounter/MainLiveDataActivityWithOnClickListeners.kt
+++ app/src/main/java/com/example/viewmodelcounter/MainLiveDataActivityWithOnClickListeners.kt
@@ -7,44 +7,52 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodelcounter.databinding.ActivityMainDatabinding2Binding
 import com.example.viewmodelcounter.databinding.ActivityMainDatabindingBinding
 
-class MainLiveDataActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainDatabindingBinding
-    lateinit var viewModel: CounterViewModel1
+class MainLiveDataActivityWithOnClickListeners : AppCompatActivity() {
+    lateinit var binding: ActivityMainDatabinding2Binding
+    lateinit var viewModel: CounterViewModel2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView<ActivityMainDatabindingBinding>(this,
-                R.layout.activity_main_databinding)
-        viewModel = ViewModelProvider(this).get(CounterViewModel1::class.java)
-        binding.viewModel = viewModel
+        binding = DataBindingUtil.setContentView<ActivityMainDatabinding2Binding>(this,
+            R.layout.activity_main_databinding2)
+        viewModel = ViewModelProvider(this).get(CounterViewModel2::class.java)
 
-        // When the value of counter's number property changes
-        // value of the text field is updated to be the new value of number
         viewModel.counter.number.observe(this, {
             Log.d("ZZZ", "viewModel.counter.number observed")
             binding.valueView.text = it.toString()
         })
+
+        binding.plusButton.setOnClickListener {
+            viewModel.counter.inc()
+        }
+        binding.minusButton.setOnClickListener {
+            viewModel.counter.dec()
+        }
     }
 }
 
-class CounterViewModel1: ViewModel() {
-    val counter = Counter1()
+class CounterViewModel2: ViewModel() {
+    val counter = Counter2()
 }
 
-class Counter1(initValue: Int = 0) {
+class Counter2(initValue: Int = 0) {
     var number: MutableLiveData<Int> = MutableLiveData()
         private set
+
     init {
         Log.d("ZZZ", "counter init")
         number.value = initValue
     }
+
     fun inc() {
         Log.d("ZZZ", "counter inc()")
         number.value = number.value?.plus(1)
     }
+
     fun dec() {
         Log.d("ZZZ", "counter dec()")
         number.value = number.value?.minus(1)
